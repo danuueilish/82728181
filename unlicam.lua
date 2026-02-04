02ffc4a7f861fb7ebf1173ff7a7ed582e0f79604
danuueilish <tegardanu10@gmail.com>
2026-02-05 06:15:16 +0700

    Create unlicam.lua

---
+++ unlicam.lua
@@ -0,0 +1,49 @@
+if _G.__UNLIMITED_CAMERA_LOADED then
+    return
+end
+_G.__UNLIMITED_CAMERA_LOADED = true
+local Players       = game:GetService("Players")
+local StarterPlayer = game:GetService("StarterPlayer")
+local LP            = Players.LocalPlayer
+local DEFAULT_MAX = StarterPlayer.CameraMaxZoomDistance
+local DEFAULT_MIN = StarterPlayer.CameraMinZoomDistance
+local MAX_ZOOM = 1e9
+local MIN_ZOOM = 0.5
+local function applyUnlimited()
+    if not LP then
+        return
+    end
+    LP.CameraMaxZoomDistance = MAX_ZOOM
+    LP.CameraMinZoomDistance = MIN_ZOOM
+end
+
+local function restoreDefault()
+    if not LP then
+        return
+    end
+    LP.CameraMaxZoomDistance = DEFAULT_MAX
+    LP.CameraMinZoomDistance = DEFAULT_MIN
+end
+
+function _G.EnableUnlimitedCamera()
+    _G.__UnlimitedCameraEnabled = true
+    applyUnlimited()
+end
+
+function _G.DisableUnlimitedCamera()
+    _G.__UnlimitedCameraEnabled = false
+    restoreDefault()
+end
+
+if LP then
+    LP:GetPropertyChangedSignal("CameraMaxZoomDistance"):Connect(function()
+        if _G.__UnlimitedCameraEnabled then
+            applyUnlimited()
+        end
+    end)
+    LP:GetPropertyChangedSignal("CameraMinZoomDistance"):Connect(function()
+        if _G.__UnlimitedCameraEnabled then
+            applyUnlimited()
+        end
+    end)
+end
